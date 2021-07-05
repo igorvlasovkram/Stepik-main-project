843002c1a75e6812cbaf3032cf153e271edf945f
Igor' Vlasov <igorkram777@gmail.com>
2021-07-05 21:48:06 +0300

    Second (and final) version of migration from one page model to another

--- stepik_main_project/page/main_page.py
+++ stepik_main_project/page/main_page.py
@@ -6,7 +6,7 @@ from stepik_main_project.page.login_page import LoginPage
 class MainPage(BasePage):
     def go_to_login_page(self):
         self.browser.find_element(*MainPageLocators.LOGIN_LINK).click()
-        return LoginPage(browser=self.browser, url=self.browser.current_url)
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK),\
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,3 +1,4 @@
+from stepik_main_project.page.login_page import LoginPage
 from stepik_main_project.page.main_page import MainPage
 
 
@@ -7,7 +8,10 @@ def test_guest_can_go_to_login_page(browser):
         'http://selenium1py.pythonanywhere.com/'
     )
     page.open()
-    login_page = page.go_to_login_page()
+
+    page.go_to_login_page()
+
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_url()
 
 
