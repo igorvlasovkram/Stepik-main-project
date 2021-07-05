c46ae477505fec64144a0c2b28c67d9a478d88b1
Igor' Vlasov <igorkram777@gmail.com>
2021-07-05 21:43:29 +0300

    First version of migration from one page model to another

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -7,8 +7,8 @@ def test_guest_can_go_to_login_page(browser):
         'http://selenium1py.pythonanywhere.com/'
     )
     page.open()
-
-    page.go_to_login_page()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_url()
 
 
 def test_guest_should_see_login_link(browser):
